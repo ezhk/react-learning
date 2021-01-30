7f7d68dc6a3375701d10d4876fb3f1875d6d0e7b
Andrey Kiselev <ezhik@ezhik.info>
2021-01-30 09:54:23 +0300

    remove unused imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import { useSelector } from "react-redux";
-import PropTypes from "prop-types";
 
 import { getProfile } from "../store/profile/selectors";
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { useSelector } from "react-redux";
-import PropTypes from "prop-types";
 
 import { getProfile } from "../store/profile/selectors";
 
